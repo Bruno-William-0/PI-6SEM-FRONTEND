8c9158ab13683b59811a11d31fb6894b0a96d500
Bruno-William-0 <Bruwill04@gmail.com>
2025-11-30 23:41:45 -0300

    Menu

--- pi-6sem-frontend/app/index.tsx
+++ pi-6sem-frontend/app/index.tsx
@@ -1,7 +1,7 @@
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { Link } from "expo-router";
 
-export default function Menu() {
+export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Menu Principal</Text>
